9862ab23e65422df7d25b383ae82a9e838e963f8
Abdalla Gaber <48131968+abdallagaber@users.noreply.github.com>
2025-05-07 20:33:07 +0300

    feat: add request interceptor to include token in Authorization header

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -7,5 +8,24 @@ const axiosInstance = axios.create({
   },
   withCredentials: true, // Important for handling cookies
 });
+
+// Add a request interceptor to include the token from cookies in the Authorization header
+// and also in a custom header that the backend can use to extract the token
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // Get the token from cookies
+    const token = Cookies.get("token");
+
+    // If token exists, add it to the Authorization header
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
