8315d278dd374463125c93eda34c6762a7df949e
Abdalla Gaber <48131968+abdallagaber@users.noreply.github.com>
2025-06-15 16:55:12 +0300

    fix: update scan type names for consistency and clarity
    - Changed "Analysis" to "Scan" in the names of various scan types for improved clarity and consistency across the application.
    - Updated names for brain, lung, kidney, retinal, and knee scans to reflect this change.

--- lib/scan-types.ts
+++ lib/scan-types.ts
@@ -1,34 +1,34 @@
 export const SCAN_TYPES = [
   {
     id: "brain-analysis",
-    name: "Brain Analysis",
+    name: "Brain Scan",
     aiModel: `${process.env.NEXT_PUBLIC_ML_API_URL}/brain-XRays/predict`,
   },
   {
     id: "lung-analysis-xray",
-    name: "Lung Analysis (X-Ray)",
+    name: "Lung Scan (X-Ray)",
     aiModel: `${process.env.NEXT_PUBLIC_ML_API_URL}/Lung-XRays/predict`,
     parentType: "lung-analysis",
   },
   {
     id: "lung-analysis-plasma",
-    name: "Lung Analysis (Plasma)",
+    name: "Lung Scan (Plasma)",
     aiModel: `${process.env.NEXT_PUBLIC_ML_API_URL}/Lung-tissues/predict`,
     parentType: "lung-analysis",
   },
   {
     id: "kidney-analysis",
-    name: "Kidney Analysis",
+    name: "Kidney Scan",
     aiModel: `${process.env.NEXT_PUBLIC_ML_API_URL}/kidney/predict`,
   },
   {
     id: "retinal-analysis",
-    name: "Retinal Analysis",
+    name: "Retinal Scan",
     aiModel: `${process.env.NEXT_PUBLIC_ML_API_URL}/Diabetic-Retinopathy/predict`,
   },
   {
     id: "knee-analysis",
-    name: "Knee Analysis",
+    name: "Knee Scan",
     aiModel: `${process.env.NEXT_PUBLIC_ML_API_URL}/Knee/predict`,
   },
 ];
